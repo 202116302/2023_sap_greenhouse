c664aa0adf0a8b1203251061821af97997f93c7b
Dayoung Kang <93754504+riverallzero@users.noreply.github.com>
2023-12-14 21:16:12 +0900

    add >> if image not include face(not a person)

--- sap-ghfr/ghfr-web.py
+++ sap-ghfr/ghfr-web.py
@@ -28,34 +28,38 @@ def main():
             saved_img_encoding = face_recognition.face_encodings(saved_img)[0]
 
             unknown_img = face_recognition.load_image_file('./unknown.jpg')
-            unknown_face_encoding = face_recognition.face_encodings(unknown_img)[0]
+            unknown_face_encoding = face_recognition.face_encodings(unknown_img)
 
-            results = face_recognition.compare_faces([saved_img_encoding], unknown_face_encoding, tolerance=0.45)
+            if len(unknown_face_encoding) >= 1:
+                unknown_face_encoding = unknown_face_encoding[0]
 
-            if results[0]:
-                conf_name = saved_img_name[0].split('.')[0].split('_')[-1]
-                date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-                df = pd.DataFrame({
-                    'timestamp': [str(date_time)],
-                    'name': [conf_name]
-                })
-
-                output_dir = './result'
-                if not os.path.exists(output_dir):
-                    os.makedirs(output_dir)
-
-                if not os.path.isfile(os.path.join(output_dir, 'manage.csv')):
-                    df.to_csv(os.path.join(output_dir, 'manage.csv'), encoding='utf-8-sig', index=False)
+                results = face_recognition.compare_faces([saved_img_encoding], unknown_face_encoding, tolerance=0.45)
+    
+                if results[0]:
+                    conf_name = saved_img_name[0].split('.')[0].split('_')[-1]
+                    date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+                    df = pd.DataFrame({
+                        'timestamp': [str(date_time)],
+                        'name': [conf_name]
+                    })
+    
+                    output_dir = './result'
+                    if not os.path.exists(output_dir):
+                        os.makedirs(output_dir)
+    
+                    if not os.path.isfile(os.path.join(output_dir, 'manage.csv')):
+                        df.to_csv(os.path.join(output_dir, 'manage.csv'), encoding='utf-8-sig', index=False)
+                    else:
+                        df_origin = pd.read_csv(os.path.join(output_dir, 'manage.csv'))
+                        df_save = pd.concat([df_origin, df])
+                        df_save.to_csv(os.path.join(output_dir, 'manage.csv'), encoding='utf-8-sig', index=False)
+    
+                    st.success(f'{conf_name}님 안녕하세요.')
                 else:
-                    df_origin = pd.read_csv(os.path.join(output_dir, 'manage.csv'))
-                    df_save = pd.concat([df_origin, df])
-                    df_save.to_csv(os.path.join(output_dir, 'manage.csv'), encoding='utf-8-sig', index=False)
-
-                st.success(f'{conf_name}님 안녕하세요.')
+                    st.warning('사용자와 일치하지 않습니다.')
             else:
                 st.warning('사용자와 일치하지 않습니다.')
-
             os.remove('./unknown.jpg')
 
 if __name__ == '__main__':
